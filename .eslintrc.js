0979d8397c796a31414a850a0d930dcbd29e0535
dev63.osasbh <pedro.silveira@osas.corp>
2022-12-15 12:55:55 -0300

    example scanner

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-  ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+        'comma-spacing': ['error', { before: false, after: true }],
+    },
 };
